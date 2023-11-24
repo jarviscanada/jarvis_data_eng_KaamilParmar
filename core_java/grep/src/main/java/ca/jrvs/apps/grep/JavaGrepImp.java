20535be05f676b81e4ff330beb5c34cec6f0dbf7
klparmar <theklparmar@gmail.com>
2023-11-24 21:02:01 +0000

    javagrepimp worked on 2 funcs done

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
@@ -4,15 +4,45 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-public class JavaGrepImp implements JavaGrep{
+import java.util.ArrayList;
+
+public class JavaGrepImp implements JavaGrep {
+
+    private String regex;
+    private String rootPath;
+    private String outFile;
+
+
     @Override
     public void process() throws IOException {
+        ArrayList<String> matchedLines = new ArrayList<>();
 
+        for(File file: listFiles(rootPath)){
+            for(String line: readLines(file)){
+                if(containsPattern(line)) matchedLines.add(line);
+            }
+        }
+        writeToFile(matchedLines);
     }
 
     @Override
     public List<File> listFiles(String rootDir) {
-        return null;
+        File current = new File(rootDir);
+        List<File> foundFiles= new ArrayList<>();
+        File[] currFiles;
+
+        if(current.isDirectory()){
+            currFiles = current.listFiles();
+            assert currFiles != null;
+            for(File file: currFiles){
+                if(file.isDirectory())
+                    listFiles(file.getAbsolutePath());
+                else if (file.isFile())
+                    foundFiles.add(file);
+            }
+        }
+
+        return foundFiles;
     }
 
     @Override
@@ -32,32 +62,32 @@ public class JavaGrepImp implements JavaGrep{
 
     @Override
     public String getRootPath() {
-        return null;
+        return this.rootPath;
     }
 
     @Override
     public void setRootPath(String rootPath) {
-
+        this.rootPath = rootPath;
     }
 
     @Override
     public String getRegex() {
-        return null;
+        return this.regex;
     }
 
     @Override
     public void setRegex(String regex) {
-
+        this.regex = regex;
     }
 
     @Override
     public String getOutFile() {
-        return null;
+        return this.outFile;
     }
 
     @Override
     public void setOutFile(String outFile) {
-
+        this.outFile = outFile;
     }
 
     public static void main(String[] args) {
