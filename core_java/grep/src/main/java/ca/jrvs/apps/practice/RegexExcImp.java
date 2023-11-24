2a3f2192f797aec32ad64daf5645af7652ef9df5
klparmar <theklparmar@gmail.com>
2023-11-24 18:49:16 +0000

    practice regex class and interface made

---
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExcImp.java
@@ -0,0 +1,9 @@
+package ca.jrvs.apps.practice;
+
+public class RegexExcImp implements RegexExc{
+    public boolean matchJpeg(String filename){return true;}
+
+    public boolean matchIp(String ip){return true;}
+
+    public boolean isEmptyLine(String line){return true;}
+}
