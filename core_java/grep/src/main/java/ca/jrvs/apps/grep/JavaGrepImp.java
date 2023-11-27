ec1fdc5ed2beb74fde6512daa99bc8f83a4cf6c6
klparmar <theklparmar@gmail.com>
2023-11-27 19:53:13 +0000

    javagrepimp done implem

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.util.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.apache.log4j.BasicConfigurator;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -15,8 +16,24 @@ public class JavaGrepImp implements JavaGrep {
     private String rootPath;
     private String outFile;
 
+    public static void main(String[] args) {
+        if(args.length != 3)
+            throw new IllegalArgumentException("USAGE: JavaGrep regex rootPath outFile");
 
+        BasicConfigurator.configure();
 
+        BasicConfigurator.configure();
+        JavaGrepImp javaGrepImp = new JavaGrepImp();
+        javaGrepImp.setRegex(args[0]);
+        javaGrepImp.setRootPath(args[1]);
+        javaGrepImp.setOutFile(args[2]);
+
+        try {
+            javaGrepImp.process();
+        } catch (Exception ex) {
+            javaGrepImp.logger.error("Error: Unable to process", ex);
+        }
+    }
 
     @Override
     public void process() throws IOException {
@@ -126,7 +143,5 @@ public class JavaGrepImp implements JavaGrep {
         this.outFile = outFile;
     }
 
-    public static void main(String[] args) {
 
-    }
 }
