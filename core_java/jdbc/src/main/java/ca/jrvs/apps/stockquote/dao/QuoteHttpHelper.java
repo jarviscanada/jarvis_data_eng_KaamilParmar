3bddbd03b2e5a0944ffd9247d14985ad472a1b82
klparmar <theklparmar@gmail.com>
2023-12-12 20:15:13 +0000

    testing httphelper and quote

--- core_java/jdbc/src/main/java/ca/jrvs/apps/stockquote/dao/QuoteHttpHelper.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/stockquote/dao/QuoteHttpHelper.java
@@ -2,6 +2,7 @@ package ca.jrvs.apps.stockquote.dao;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import okhttp3.OkHttpClient;
 
 import java.io.IOException;
@@ -32,6 +33,7 @@ public class QuoteHttpHelper {
      * @throws IllegalArgumentException - if no data was found for the given symbol
      */
     public Quote fetchQuoteInfo(String ticker) throws IllegalArgumentException {
+        Quote quote = new Quote();
 
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("https://alpha-vantage.p.rapidapi.com/query?function=GLOBAL_QUOTE&symbol="+ticker+"&datatype=json"))
@@ -41,7 +43,12 @@ public class QuoteHttpHelper {
                 .build();
         try {
             HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println(response.body());
+            String JSONresp = response.body();
+            ObjectMapper om = new ObjectMapper();
+
+            quote = om.readValue(JSONresp, Quote.class);
+
+            System.out.println(response.body()+"\n e"+ quote.getTicker());
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (JsonMappingException e) {
